18afcb5446fce2adfe2754a0ba0a5ad62aa95dd5
Aarjan Khatiwada <aarjankhatiwada707@gmail.com>
2025-12-05 20:06:49 -0700

    final peoject

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Function #1: used on Home page button
+// Shows a simple alert with a fake daily student deal
+function showStudentDeal() {
+    alert("Today’s sample deal: 10% off all styles for students! (Code: STUDENT10)");
+}
+
+// Function #2: used on Contact page form
+// Confirms that the user really wants to submit their message
+function confirmFormSubmit() {
+    return confirm("Do you want to submit this message? (This is just a class demo.)");
+}
